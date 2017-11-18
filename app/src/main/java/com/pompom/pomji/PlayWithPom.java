96ffa6fa6e883b590301f4ab18747520b4ac35de
DOS <pisol_ruenin@cmu.ac.th>
2017-11-18 18:26:00 +0700

    play pom animation

--- app/src/main/java/com/pompom/pomji/PlayWithPom.java
+++ app/src/main/java/com/pompom/pomji/PlayWithPom.java
@@ -8,10 +8,15 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PlayWithPom extends AppCompatActivity implements SensorEventListener {
 
@@ -19,16 +24,46 @@ public class PlayWithPom extends AppCompatActivity implements SensorEventListene
 
     TextView steps;
 
-    float count=0;
+    float count = 0;
 
     boolean running = false;
+
+    boolean change = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_with_pom);
-        steps = (TextView)findViewById(R.id.steps);
+        steps = (TextView) findViewById(R.id.steps);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SharedPreferences shared = getSharedPreferences("my_ref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = shared.edit();
+        Gson gson = new Gson();
+        final User user;
+        String json = shared.getString("User", "");
+        user = gson.fromJson(json, User.class);
+
+        ProgressBar funbar = (ProgressBar) findViewById(R.id.funbar);
+        final ImageView pom = (ImageView) findViewById(R.id.pom);
+
+        FunBar funBar = new FunBar(funbar);
+
+        funBar.decrease(shared);
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        change = user.getPom().Move(change, pom);
+                    }
+                });
+            }
+        }, 0, 180);
+
 
     }
 
@@ -47,15 +82,15 @@ public class PlayWithPom extends AppCompatActivity implements SensorEventListene
     @Override
     protected void onPause() {
         super.onPause();
-        SharedPreferences shared = getSharedPreferences("my_ref",MODE_PRIVATE);
+        SharedPreferences shared = getSharedPreferences("my_ref", MODE_PRIVATE);
         SharedPreferences.Editor editor = shared.edit();
         Gson gson = new Gson();
         String json = shared.getString("User", "");
         User user = gson.fromJson(json, User.class);
-        user.getPom().setFun((int)count/5);
+        user.getPom().setFun((int) count / 5);
         json = gson.toJson(user);
         editor.putString("User", json);
-        editor.putFloat("step",count);
+        editor.putFloat("step", count);
         editor.commit();
         running = false;
         sensorManager.unregisterListener(this);
@@ -63,15 +98,15 @@ public class PlayWithPom extends AppCompatActivity implements SensorEventListene
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        if(running){
+        if (running) {
 
-            SharedPreferences shared = getSharedPreferences("my_ref",MODE_PRIVATE);
+            SharedPreferences shared = getSharedPreferences("my_ref", MODE_PRIVATE);
             SharedPreferences.Editor editor = shared.edit();
-            if(shared.getFloat("step",0)==0){
-                editor.putFloat("step",0);
+            if (shared.getFloat("step", 0) == 0) {
+                editor.putFloat("step", 0);
                 editor.commit();
             }
-            steps.setText(String.valueOf(sensorEvent.values[0]-shared.getFloat("step",0)));
+            steps.setText(String.valueOf(sensorEvent.values[0] - shared.getFloat("step", 0)));
             count = sensorEvent.values[0];
         }
     }
