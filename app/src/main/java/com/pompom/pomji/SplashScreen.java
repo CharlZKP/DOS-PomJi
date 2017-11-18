21a570a2e4c9cb80d3fd889a05793db5cfe5caf7
DOS <pisol_ruenin@cmu.ac.th>
2017-11-18 17:45:24 +0700

    splash screen animation

--- app/src/main/java/com/pompom/pomji/SplashScreen.java
+++ app/src/main/java/com/pompom/pomji/SplashScreen.java
@@ -1,8 +1,11 @@
 package com.pompom.pomji;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,10 +13,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ImageView imgLogo = (ImageView) findViewById(R.id.logo);
+        ObjectAnimator logo = ObjectAnimator.ofFloat(imgLogo,"translationY",-1500,0);
+        logo.setDuration(1300);
+        ObjectAnimator logo2 = ObjectAnimator.ofFloat(imgLogo,"rotation",0,360);
+        logo2.setDuration(1300);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(logo,logo2);
+        animatorSet.start();
         Thread t = new Thread() {
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(5000);
                     Intent intent = new Intent(getApplicationContext(), Intro.class);
                     startActivity(intent);
                     finish();
